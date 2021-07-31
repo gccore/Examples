8d50889c8210153e11bc113b6f81a27052939063
Ghasem Ramezani <g1999ramezani@gmail.com>
2021-08-01 00:29:01 +0430

    - multi-key support added to move_qbutton example.
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

--- sub_examples/move_qbutton/source/main.cpp
+++ sub_examples/move_qbutton/source/main.cpp
@@ -22,15 +22,24 @@
 #include <QPointer>
 #include <QDialog>
 
+#include <bitset>
+
 namespace core::constants {
 auto constexpr width = 400;
 auto constexpr height = 300;
 auto constexpr step = 2;
+auto constexpr super_step = 4;
+auto constexpr key_length = 5;
 } // namespace core::constants
+
+namespace core::keys {
+enum key_types { w, a, s, d, shift };
+} // namespace core::keys
 
 namespace core {
 struct my_widget final: QDialog {
   QPointer<QPushButton> button = nullptr;
+  std::bitset<constants::key_length> pressed;
 
   my_widget()
     : QDialog(nullptr),
@@ -38,19 +47,55 @@ struct my_widget final: QDialog {
     setMinimumSize(constants::width, constants::height);
   }
 
-  void keyPressEvent(QKeyEvent* const event) override {
-    if (QKeyEvent::KeyPress == event->type()) {
-      if (Qt::Key_W == event->key()) {
-        button->move(button->x(), button->y() - constants::step);
-      } else if (Qt::Key_S == event->key()) {
-        button->move(button->x(), button->y() + constants::step);
-      } else if (Qt::Key_D == event->key()) {
-        button->move(button->x() + constants::step, button->y());
-      } else if (Qt::Key_A == event->key()) {
-        button->move(button->x() - constants::step, button->y());
-      }
+  using key_t = int;
+  void toggle_button(key_t const key, bool const state) {
+    if (Qt::Key_Shift == key) {
+      pressed.set(keys::shift, state);
+    } else if (Qt::Key_W == key) {
+      pressed.set(keys::w, state);
+    } else if (Qt::Key_S == key) {
+      pressed.set(keys::s, state);
+    } else if (Qt::Key_D == key) {
+      pressed.set(keys::d, state);
+    } else if (Qt::Key_A == key) {
+      pressed.set(keys::a, state);
     }
+  }
+  void set_pressed(key_t const key) {
+    toggle_button(key, true);
+  }
+  void set_released(key_t const key) {
+    toggle_button(key, false);
+  }
+
+  void move_button() {
+    auto step = constants::step;
+    if (pressed.test(keys::shift)) {
+      step *= constants::super_step;
+    }
+    if (pressed.test(keys::w)) {
+      button->move(button->x(), button->y() - step);
+    }
+    if (pressed.test(keys::s)) {
+      button->move(button->x(), button->y() + step);
+    }
+    if (pressed.test(keys::d)) {
+      button->move(button->x() + step, button->y());
+    }
+    if (pressed.test(keys::a)) {
+      button->move(button->x() - step, button->y());
+    }
+  }
+
+  void keyPressEvent(QKeyEvent* const event) override {
+    set_pressed(event->key());
+    move_button();
     QDialog::keyPressEvent(event);
+  }
+  void keyReleaseEvent(QKeyEvent* const event) override {
+    set_released(event->key());
+    move_button();
+    QDialog::keyReleaseEvent(event);
   }
 };
 } // namespace core
