17b02b5711c6c53628304c35240393f612ab4bc4
Ghasem Ramezani <g1999ramezani@gmail.com>
2021-08-29 19:20:23 +0430

    - vector_to_number_i example added.
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

---
+++ sub_examples/vector_to_number_i/source/main.cpp
@@ -0,0 +1,51 @@
+/*
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @another_ghasem
+ * g1999ramezani@gmail.com
+ */
+
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <memory>
+
+#include "gmpxx.h"
+
+namespace core
+{
+static mpz_class convert_to_i(std::vector<std::size_t> const& vec)
+{
+        mpz_class result = 1;
+        for (auto const number : vec) {
+                result *= number;
+        }
+        return result;
+}
+} // namespace core
+
+static std::ostream& operator<<(std::ostream& out, mpz_class const& in)
+{
+        return out << in.get_str();
+}
+
+int main()
+{
+        std::vector<std::size_t> const vec = {
+                16377,
+                2631694347470643681,
+                1173029487328219238
+        };
+        std::cout << core::convert_to_i(vec) << std::endl;
+}
