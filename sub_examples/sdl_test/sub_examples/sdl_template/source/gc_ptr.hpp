e486f34f6469e00315468ef5486a8fc87ec34984
Ghasem Ramezani <g1999ramezani@gmail.com>
2021-09-08 23:09:25 +0430

    - sdl_test example updated:         - gc_ptr added as a observer_ptr.
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

---
+++ sub_examples/sdl_test/sub_examples/sdl_template/source/gc_ptr.hpp
@@ -0,0 +1,98 @@
+#ifndef GC_SDL_BASICS_GC_PTR_HPP
+#define GC_SDL_BASICS_GC_PTR_HPP
+
+namespace core
+{
+template <typename T>
+class gc_ptr final
+{
+	T* ptr = nullptr;
+
+public:
+	gc_ptr() noexcept
+		: ptr(nullptr)
+	{
+	}
+	explicit gc_ptr(T& in) noexcept
+		: ptr(&in)
+	{
+	}
+	explicit gc_ptr(T* in) noexcept
+		: ptr(in)
+	{
+	}
+	gc_ptr(decltype(nullptr)) noexcept
+		: ptr(nullptr)
+	{
+	}
+	explicit gc_ptr(gc_ptr<T> const& other) noexcept
+		: ptr(other.ptr)
+	{
+	}
+	explicit gc_ptr(gc_ptr<T>&& other) noexcept
+		: ptr(other.ptr)
+	{
+		other.ptr = nullptr;
+	}
+
+	T& operator*() noexcept
+	{
+		return *ptr;
+	}
+	T const& operator*() const noexcept
+	{
+		return *ptr;
+	}
+	gc_ptr<T>& operator=(gc_ptr<T> const& other) noexcept
+	{
+		ptr = other.ptr;
+		return *this;
+	}
+	gc_ptr<T>& operator=(gc_ptr<T>&& other) noexcept
+	{
+		ptr = other.ptr;
+		other.ptr = nullptr;
+		return *this;
+	}
+	gc_ptr<T>& operator=(decltype(nullptr)) noexcept
+	{
+		ptr = nullptr;
+		return *this;
+	}
+	T* operator->() noexcept
+	{
+		return ptr;
+	}
+	T const* operator->() const noexcept
+	{
+		return ptr;
+	}
+	bool operator!=(gc_ptr<T> const& other) const noexcept
+	{
+		return other.ptr != ptr;
+	}
+	bool operator!=(decltype(nullptr)) const noexcept
+	{
+		return nullptr != ptr;
+	}
+	friend bool operator!=(decltype(nullptr), gc_ptr<T> const& other) noexcept
+	{
+		return nullptr != other.ptr;
+	}
+
+	bool is_null() const noexcept
+	{
+		return nullptr != ptr;
+	}
+	T* data() noexcept
+	{
+		return ptr;
+	}
+	T const* data() const noexcept
+	{
+		return ptr;
+	}
+};
+} // namespace core
+
+#endif // GC_SDL_BASICS_GC_PTR_HPP
