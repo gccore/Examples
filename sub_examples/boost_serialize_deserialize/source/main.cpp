b6c39acb3e07553212835ef5ff5b1525ad0d4977
Ghasem Ramezani <g1999ramezani@gmail.com>
2022-08-09 03:53:28 +0430

    Fix Build Error: boost_serialize_deserialize
    Signed-off-by: Ghasem Ramezani (Personal GCCORE GPG) <g1999ramezani@gmail.com>

--- sub_examples/boost_serialize_deserialize/source/main.cpp
+++ sub_examples/boost_serialize_deserialize/source/main.cpp
@@ -16,66 +16,58 @@
  * g1999ramezani@gmail.com
  */
 
-#include <filesystem>
-#include <iostream>
-#include <fstream>
-#include <string>
-
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/serialization/deque.hpp>
+//
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
 
-struct my_structure final
-{
-	friend class boost::serialization::access;
+struct my_structure final {
+  friend class boost::serialization::access;
 
-	std::deque<std::string> data;
+  std::deque<std::string> data;
 
-	template<class Archive>
-	void serialize(Archive& ar, unsigned int const)
-	{
-		ar& data;
-	}
+  template <class Archive>
+  void serialize(Archive& ar, unsigned int const) {
+    ar& data;
+  }
 };
 
-void save(my_structure const& obj, std::filesystem::path const& path)
-{
-	std::ofstream output(path);
-	if(!output.is_open())
-	{
-		throw std::runtime_error("Couldn't open file: " + path.string());
-	}
+void save(my_structure const& obj, std::filesystem::path const& path) {
+  std::ofstream output(path);
+  if (!output.is_open()) {
+    throw std::runtime_error("Couldn't open file: " + path.string());
+  }
 
-	boost::archive::text_oarchive boutput(output);
-	boutput << obj;
+  boost::archive::text_oarchive boutput(output);
+  boutput << obj;
 }
 
-auto load(std::filesystem::path const& path)
-{
-	std::ifstream input(path);
-	if(!input.is_open())
-	{
-		throw std::runtime_error("Couldn't open file: " + path.string());
-	}
+auto load(std::filesystem::path const& path) {
+  std::ifstream input(path);
+  if (!input.is_open()) {
+    throw std::runtime_error("Couldn't open file: " + path.string());
+  }
 
-	boost::archive::text_iarchive binput(input);
-	my_structure result;
-	binput >> result;
-	return result;
+  boost::archive::text_iarchive binput(input);
+  my_structure result;
+  binput >> result;
+  return result;
 }
 
-int main()
-{
-	my_structure d;
-	d.data.push_back("1");
-	d.data.push_back("2");
-	d.data.push_back("3");
+int main() {
+  my_structure d;
+  d.data.push_back("1");
+  d.data.push_back("2");
+  d.data.push_back("3");
 
-	save(d, "backup");
-	auto const d2 = load("backup");
+  save(d, "backup");
+  auto const d2 = load("backup");
 
-	for(auto const& item : d2.data)
-	{
-		std::cout << item << std::endl;
-	}
+  for (auto const& item : d2.data) {
+    std::cout << item << std::endl;
+  }
 }
